87b88ac6dfd494e384bd2b9b050d47c23ebd6a38
Sutiatii <sutiati08@gmail.com>
2026-02-27 15:08:18 +0000

    Perbaikan

--- js/profile.js
+++ js/profile.js
@@ -4,9 +4,13 @@ window.onload = function(){
     let collage = prompt("Enter Your Collage:");
     let profession = prompt("Enter Your Profession:");
     let skill = prompt("Enter Your Skill:");
+    let visimisi = prompt("Enter Your Visi and Mission:");
+    let company = prompt("Enter About Your Company:");
     document.getElementById('res-name').innerText =": " + (name || "-");
     document.getElementById('res-age').innerText =": " + (age || "-");
     document.getElementById('res-collage').innerText =": " + (collage || "-");
     document.getElementById('res-profession').innerText =": " + (profession || "-");
     document.getElementById('res-skill').innerText =": " + (skill || "-");
+    document.getElementById('res-visimisi').innerText =": " + (visimisi || "-");
+    document.getElementById('res-company').innerText =": " + (company || "-");
 }
